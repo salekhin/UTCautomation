d664201f9896007a56f869111c5e4c98e64eb646
salekhin <sergey.alekhin@dataart.com>
2017-12-12 13:46:23 +0200

    testCommit

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -12,7 +12,6 @@ import static org.fest.assertions.Assertions.assertThat;
  * Created by salekhin on 12.12.2017.
  */
 
-//firstttt
 public class FirstTest extends BaseTest {
     private LoginPage loginPage;
     private Homepage homepage;
